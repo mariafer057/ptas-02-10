153ea2407679ea120f2592a728cd1e037b3bb1fc
mariafer057 <mariafernandaalves057@gmail.com>
2024-11-12 19:23:25 -0400

    12/11

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,32 +1,29 @@
-const {PrismaClient} = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../prisma/prismaClient");
+require('dotenv').config();
 
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 class AuthController{
-
     static async cadastro(req, res){
-        const { nome, email, password, tipo } = req.body;
+        const {nome, email, password, tipo} = req.body;
 
-        if(!nome || nome.lenght < 6){
-            return res.json({
+        if(!nome || nome.length < 6){
+            return res.status(422).json({
                 erro: true,
                 mensagem: "O nome deve ter pelo menos 6 caracteres."
             });
         }
-
-        if(!email || email.lenght < 10){
-            return res.json({
+        if(!email || email.length < 10){
+            return res.status(422).json({
                 erro: true,
                 mensagem: "O email deve ter pelo menos 10 caracteres."
             });
         }
-
-        if(!password || password.lenght < 10){
-            return res.json({
+        if(!password || password.length < 8){
+            return res.status(422).json({
                 erro: true,
-                mensagem: "O password deve ter pelo menos 8 caracteres."
+                mensagem: "A senha deve ter pelo menos 8 caracteres."
             });
         }
 
@@ -36,86 +33,76 @@ class AuthController{
             },
         });
 
-
-        if (existe != 0) {
-            return res.json({
+        if(existe != 0){
+            return res.status(422).json({
                 erro: true,
-                mensagem: "Já existe um user com esse email",
+                mensagem: "Já existe usuário cadastrado com este e-mail"
             });
         }
 
-
-        const salt = bcryptjs.genSaltSync(123);
+        const salt = bcryptjs.genSaltSync(8);
         const hashPassword = bcryptjs.hashSync(password, salt);
 
-
         try{
-       const usuario = await prisma.usuario.create({
-            data: {
-                nome: nome,
-                email: email,
-                password: hashPassword,
-                tipo: "cliente",
-            },
-        });
+            const usuario = await prisma.usuario.create({
+                data: {
+                    nome: nome,
+                    email: email,
+                    password: hashPassword,
+                    tipo: "cliente",
+                },
+            });
 
-        console.log(JSON.stringify(usuario));
-        
-        const token = jwt.sign({id: usuario.id}, process.env.SECRET_KEY, {
-            expiresIn: "1h",
-        });
-
-        return res.json({
-            erro: false,
-            mensagem: "user cadastrado com sucesso!",
-            token: token
-
-        });
-
-    } catch (error){
-        return res.json({
-            erro: true,
-            mensagem: "Ocorreu um erro, tente novamente mais tarde!" + error,
-        });
-    }
-    
+        return res.status(201).json({
+        erro: false,
+        mensagem: "Usuário cadastrado com sucesso!",
+       });
+        } catch (error){
+            return res.status(500).json({
+                erro: true,
+                mensagem: "Ocorreu um erro, tente novamente mais tarde!" + error,
+               }); 
+        }
 
     }
-
     static async login(req, res){
-        const {email, password} = req.body
+        const {email, password} = req.body;
 
         const usuario = await prisma.usuario.findUnique({
             where: {
-                email: email
-            }
+                email: email,
+            },
         });
 
         if(!usuario){
-            return res.json({
+            return res.status(422).json({
                 erro: true,
-                mensagem: "User não encontrado",
+                mensagem: "Usuário não encontrado.",
             });
         }
 
-        //verificar senha
+        //Verificação da senha
         const senhaCorreta = bcryptjs.compareSync(password, usuario.password);
 
         if(!senhaCorreta){
-            return res.json({
+            return res.status(422).json({
                 erro: true,
                 mensagem: "Senha incorreta.",
-            });
+            })
+        };
 
-            const token = jwt.sign({id: usuario.id}, process.env.SECRET_KEY, {
-                expiresIn: "1h",
-            });
-            res.json({
-                erro: false,
-                mensagem: "Autenticação realizada com sucesso!",
-                token: token,
-            });
-        }
+        const token = jwt.sign({id: usuario.id}, process.env.SECRET_KEY, {
+            expiresIn: "1h"
+        });
+
+        res.status(200).json({
+            erro: false,
+            mensagem: "Autenticação realizada com sucesso!",
+            token: token,
+            
+        })
     }
+    
 }
+
 module.exports = AuthController;
